38f8e7f4a12f063d77046c8a49baea7c96d0c15a
Alejandro Otalvaro <jaom2479@gmail.com>
2023-12-12 15:01:42 -0500

    update
    update

---
+++ resources/views/home/create.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div>
+            <form action="" method="post">
+                
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div>
+            <ul>
+                <li>
+                    <a href="#">Crear Petición</a>
+                </li>
+                <li>
+                    <a href="#">Ver Peticiones</a>
+                </li>
+                <li>
+                    <a href="#">Actualizar Petición</a>
+                </li>
+                <li>
+                    <a href="#">Eliminar Petición</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+@endsection
